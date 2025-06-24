cff784268f37a26aa221790518da4052461263ef
BuildByTushar <tusharchoudharyxpace@gmail.com>
2025-06-24 21:15:59 +0530

    24 jine 9:13 pm

--- Chapter 5/Dictionary method.py
+++ Chapter 5/Dictionary method.py
@@ -15,3 +15,13 @@ print(Marks.items())
 
 print(Marks.get("Tushar"))   # If key doesn't exist it gives "None"
 print(Marks["Tushar"])  # WE DOES NOT PREFER it as it gives an ERROR if key doesn't exist
+
+
+Test_Dic = Marks.copy()
+print(Test_Dic.items()) 
+
+Test_Dic.pop("Ujjwal","Not Found")
+print(Test_Dic.items())
+
+Test_Dic.clear()  # it will clear our whole dictionary
+print(Test_Dic.items())
