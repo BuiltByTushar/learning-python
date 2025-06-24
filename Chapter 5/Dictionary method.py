63a3755c553ebddacd89ed92907daeb2fcaaec51
BuiltByTushar <tusharchoudharyxpace@gmail.com>
2025-06-24 20:32:09 +0530

    Create Dictionary method.py

---
+++ Chapter 5/Dictionary method.py
@@ -0,0 +1,17 @@
+Marks = {
+    "Rohan":67,
+    "Harry":98,
+    "Tushar":100,
+    "Ujjwal":97,
+    "Mohit":38,
+    "Arnav":58
+}
+print(Marks.items())
+print(Marks.keys())
+print(Marks.values())
+
+Marks.update({"Tushar":99})
+print(Marks.items())
+
+print(Marks.get("Tushar"))   # If key doesn't exist it gives "None"
+print(Marks["Tushar"])  # WE DOES NOT PREFER it as it gives an ERROR if key doesn't exist
