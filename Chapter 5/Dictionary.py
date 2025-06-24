5f760dd490c10abcbb0e248df8350cdd1dbd22fd
BuiltByTushar <tusharchoudharyxpace@gmail.com>
2025-06-24 20:27:31 +0530

    Create Dictionary.py

---
+++ Chapter 5/Dictionary.py
@@ -0,0 +1,13 @@
+Dictionary = {"key":"value",
+              "Tushar":"Physics",
+              "marks":"100",
+              "list":[1,2,9]
+}
+print(Dictionary["key"])
+print(Dictionary["list"])
+# print(Dictionary["100"])      #THIS IS NOT posible 
+
+# it is unordered
+# it is mutable
+# it is indexed
+# connot contain duplicate keys
