4b480be5d9db6ef88f4f54ceba7055a9137b1c1b
BuildByTushar <tusharchoudharyxpace@gmail.com>
2025-07-01 11:03:23 +0530

    loops

---
+++ Chapter 7/Loops.py
@@ -0,0 +1,25 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# OR
+
+for x in range(1,6):
+    print(x)
+
+# OR
+
+x = 1
+
+while x <= 5:
+    print(x)
+    x += 1
+
+x = 1
+
+while x <=5:
+    print("*"*x)
+    x += 1
+
