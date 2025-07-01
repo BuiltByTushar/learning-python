7eb8994b861c469f4f4c1c6940835a3a761c6b70
BuildByTushar <tusharchoudharyxpace@gmail.com>
2025-07-01 21:00:26 +0530

    1 July

---
+++ Chapter 7/For loop.py
@@ -0,0 +1,40 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for x in list :
+    print(x)
+
+tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+for x in tuple :
+    print(x)
+
+
+# Here Set can also be printed
+set = {1, 2, 3 ,4, 5, 6, 7, 8, 9, 10}
+
+for x in set:
+    print(x)
+
+### If we want to make a list from set 
+
+list_set = []
+for x in set:
+    list_set.append(x)
+
+print(list_set)
+print(type(list_set))  # It is a list
+
+
+x = "*"
+
+for x in range(1,6,2):  # Range(start,end,skip_value)
+    print(x*"*")
+
+# Itteration
+name = "Tushar"
+
+for x in name:
+    print(x)
+else:
+    print("Done")
+
