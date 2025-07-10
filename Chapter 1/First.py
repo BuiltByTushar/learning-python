2736dd6e0ba5d553411c57cd12a8d790f5784e49
BuildByTushar <tusharchoudharyxpace@gmail.com>
2025-07-10 13:10:15 +0530

    new

--- Chapter 1/First.py
+++ Chapter 1/First.py
@@ -1,10 +1,10 @@
-"""
+
 import pyjokes
 
 # This prints bad jokes
 joke = pyjokes.get_joke()
 print(joke)
-"""
+
 """
 This is my first code
 Thank you
--- Chapter 1/Practice set 1.py
+++ Chapter 1/Practice set 1.py
@@ -29,13 +29,13 @@ Twinkle, twinkle, little star.''')
 # Question 3.
 # Install an external module and use it to perform an operation of your interest.
 
-'''
+
 import wikipedia
 
 result = wikipedia.page("Minecraft")
 
 print(result.summary)
-'''
+
 # Question 4. 
 # Write a python program to print the contents of a directory using the os module.
 # Search online for the function which does that
